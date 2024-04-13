c223be543c5b54f51f1002b469d6918bed1a4034
Mujahid002 <mujahidshaik2002@gmail.com>
2024-04-13 22:15:51 +0530

    updated

---
+++ backend/database/index.js
@@ -0,0 +1,7 @@
+const connectMongo = require("./connect-mongo");
+
+const storeBlogAttest = async () => {};
+
+const fetchBlogAttest = async () => {};
+
+module.exports = { storeBlogAttest, fetchBlogAttest };
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,16 @@
 const express = require("express");
-const ethers = require("ethers");
 const connectMongo = require("./database/connect-mongo");
 const cors = require("cors");
 
 const app = express();
-
+const { storeBlogAttest, fetchBlogAttest } = require("./database/index");
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get("/fetch-knowledge-prices", fetchPrice);
-// app.get("/fetch-knowledge-prices", fetchPrice);
-// app.post("/raise-proposal", storeRaisedProposal);
-// app.get("/fetch-proposals", fetchProposalsForProposalIds);
+app.get("/fetch-blog-attests", fetchBlogAttest);
+app.post("/store-attest", storeBlogAttest);
 
 const port = process.env.PORT || 7001;
 app.listen(port, async () => {
