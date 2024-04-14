b5bcc9933b9b822cc003d30d4ccaa6780e01c457
Mujahid002 <mujahidshaik2002@gmail.com>
2024-04-14 16:12:43 +0530

    updated mongo functions

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,7 +1,23 @@
 const connectMongo = require("./connect-mongo");
 
-const storeBlogAttest = async () => {};
+const storeBlogAttest = async (attestData) => {
+  const client = await connectMongo();
+  const db = client.db("attest");
+  const collection = db.collection("blog");
+  await collection.insertOne({ attestData });
+  await client.close();
+};
 
-const fetchBlogAttest = async () => {};
+const fetchBlogAttest = async () => {
+  const client = await connectMongo();
+  const db = client.db("attest");
+  const collection = db.collection("blog");
+
+  const blogAttents = await collection.find({}).toArray();
+
+  await client.close();
+
+  return blogAttents;
+};
 
 module.exports = { storeBlogAttest, fetchBlogAttest };
