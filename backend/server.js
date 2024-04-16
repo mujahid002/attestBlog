484e99e53974447d19e877e36655e979607ab6b5
Mujahid002 <mujahidshaik2002@gmail.com>
2024-04-16 17:38:49 +0530

    added endpoints

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", async (req, res) => {
+  res.status(200).send("Working on 7001 PORT!");
+});
 app.get("/fetch-blog-attests", async (req, res) => {
   try {
     const blogAttests = await fetchBlogAttest();
