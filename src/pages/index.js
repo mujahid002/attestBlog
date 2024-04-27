25b79af0168cfcbfe586667ac201dd62e4cf6e59
Mujahid002 <mujahidshaik2002@gmail.com>
2024-04-27 13:17:41 +0530

    updated components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 // import { attest } from "@/components/attest";
-import {
-  EAS as EAS150,
-  SchemaEncoder,
-} from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from "ethers";
+
 import axios from "axios";
+
+// import { connectWallet } from "@/components/connectWallet";
+import { attest } from "@/components/attest";
+import { handleUploadToPinata } from "@/components/pinata";
+import { submitPassport } from "@/components/submitPassport";
 
 export default function Home() {
   const [address, setAddress] = useState("");
-
+  const [eligible, setEligible] = useState(false);
   async function connectWallet() {
     try {
       if (!window.ethereum) {
@@ -87,152 +89,6 @@ export default function Home() {
     }
   }
 
-  const attest = async (userAddress, blogId) => {
-    try {
-      if (!userAddress || !blogId) {
-        throw new Error("Invalid input parameters");
-      }
-
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      console.log("Provider:", provider);
-
-      await window.ethereum.request({ method: "eth_requestAccounts" });
-
-      const signer = await provider.getSigner(address);
-      console.log("Signer:", signer);
-
-      // const txn = await signer.sendTransaction({
-      //   to: "0x1c620232Fe5Ab700Cc65bBb4Ebdf15aFFe96e1B5",
-      //   value: "10000000000000000",
-      // });
-
-      // console.log("Sent 0.01 to 0x1c", txn.hash);
-
-      const easContractAddress = "0x4200000000000000000000000000000000000021";
-      const schemaUID =
-        "0x08cf4bbd043399f5b4ac08c48204c0f4cd7a3cb939ec7a1da08cb5e010e65193";
-
-      // Initialize EAS instance
-      const eas = new EAS150(easContractAddress);
-
-      // Connect signer to EAS instance
-      eas.connect(signer);
-
-      // Initialize SchemaEncoder with the schema string
-      const schemaEncoder = new SchemaEncoder(
-        "address Creator, string ContentID"
-      );
-      const encodedData = schemaEncoder.encodeData([
-        { name: "Creator", value: userAddress, type: "address" },
-        { name: "ContentID", value: blogId, type: "string" },
-      ]);
-
-      // Attest the data
-      const tx = await eas.attest({
-        schema: schemaUID, // Replace with your schema identifier
-        data: {
-          recipient: userAddress,
-          // expirationTime: 0,
-          revocable: true,
-          data: encodedData,
-        },
-      });
-
-      const newAttestationUID = await tx.wait();
-
-      console.log("New attestation UID:", newAttestationUID);
-
-      // Store the attestation data (assuming axios exists)
-      const data = {
-        owner: userAddress,
-        cid: blogId,
-        attestUID: newAttestationUID,
-      };
-
-      const res = await axios.post(
-        "http://localhost:7001/store-attest", // Replace with your backend URL
-        {
-          attestData: data,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (res.status === 200) {
-        return "Success"; // Indicate success
-      } else {
-        throw new Error("Internal Server Error");
-      }
-    } catch (error) {
-      console.error("Unable to run Attest: ", error);
-      throw error; // Rethrow the error for handling in the calling function
-    }
-  };
-
-  const handleUploadToPinata = async (fileName, data) => {
-    try {
-      if (!data) {
-        throw new Error("Invalid Data!");
-      }
-
-      const blobData = new Blob([data], { type: "application/json" });
-
-      const form = new FormData();
-      form.append("file", blobData, "data.json");
-
-      const metadata = JSON.stringify({
-        name: fileName,
-      });
-      form.append("pinataMetadata", metadata);
-
-      const options = JSON.stringify({
-        cidVersion: 0,
-      });
-      form.append("pinataOptions", options);
-
-      const res = await fetch(
-        "https://api.pinata.cloud/pinning/pinFileToIPFS",
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_PINATA_JWT}`,
-          },
-          body: form,
-        }
-      );
-
-      if (!res.ok) {
-        throw new Error(`Failed to upload file to Pinata: ${res.statusText}`);
-      }
-
-      const resData = await res.json();
-
-      console.log("the response is: ", resData.IpfsHash);
-
-      return resData.IpfsHash;
-    } catch (error) {
-      console.error(error);
-      throw new Error("Unable to upload file to Pinata");
-    }
-  };
-
-  // const [blogTitle, setBlogTitle] = useState("");
-  // const [blogCategory, setBlogCategory] = useState("");
-  // const [blogContent, setBlogContent] = useState("");
-
-  // const handleInputChange = (event, setterFunction) => {
-  //   setterFunction(event.target.value);
-  // };
-
-  // const adjustInputSize = (event) => {
-  //   const inputElement = event.target;
-  //   inputElement.style.height = "auto";
-  //   inputElement.style.height = inputElement.scrollHeight + "px";
-  // };
-
   const attestBlog = async () => {
     try {
       const blogTitle = document.getElementById("blogTitle").value;
@@ -285,7 +141,40 @@ export default function Home() {
   useEffect(() => {
     connectWallet();
     fetchUserData();
+    setEligible(false);
   }, [address]);
+
+  const handlePassport = async () => {
+    try {
+      const scorerId = document.getElementById("scorerId").value;
+      console.log("The scorer Id is", scorerId);
+      console.log("The address is", address);
+      const data = await submitPassport(address, scorerId);
+
+      if (data.status === "DONE") {
+        if (data.score == 0) {
+          alert(
+            `Follow this link https://passport.gitcoin.co/#/ to increase your score for ${address}`
+          );
+        } else if (data.score > 5) {
+          setEligible(true);
+        } else {
+          alert("Your Score should be greater than 5 To Write Blogs!");
+        }
+      } else {
+        alert("Please Submit Your Passport correctly!");
+      }
+    } catch (error) {
+      if (error.message === "Incorrect scorer ID provided.") {
+        alert("Incorrect scorer ID provided. Please check and try again.");
+      } else {
+        // Handle other types of errors
+        alert(
+          "An error occurred while processing your request. Please try again later."
+        );
+      }
+    }
+  };
 
   return (
     <div className="text-black min-h-screen flex justify-center items-center bg-gray-100">
@@ -304,42 +193,71 @@ export default function Home() {
             <p className="text-gray-700 text-lg">{address}</p>
           )}
 
-          <span id="userAddress" className="text-gray-700 mb-4"></span>
-          <div className="flex space-x-4">
-            {/* Blog Content form */}
-            <div className="flex flex-col text-black">
+          <div class="py-1 flex space-x-4">
+            <div class="relative flex flex-col text-black">
               <input
                 type="text"
-                id="blogTitle"
-                placeholder="Blog Title"
-                // onChange={(event) => handleInputChange(event, setBlogTitle)}
-                className="rounded-md border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500 mb-2"
-                required
-              />
-              <input
-                type="text"
-                id="blogCategory"
-                placeholder="Blog Category"
-                // onChange={(event) => handleInputChange(event, setBlogCategory)}
-                className="rounded-md border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500 mb-2"
-                required
-              />
-              <input
-                type="text"
-                id="blogContent"
-                placeholder="Blog Content"
-                className="rounded-md border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500 mb-2"
-                style={{ minHeight: "100px" }}
+                id="scorerId"
+                placeholder="Enter your Scorer ID"
+                class="rounded-md border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500 mb-2"
                 required
               />
               <button
-                id="sendMailButton"
-                className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                onClick={attestBlog}
+                id="submitPassport"
+                class="relative bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={() => {
+                  handlePassport();
+                }}
               >
-                Attest Blog
+                Submit Your Passport Only Once!
               </button>
             </div>
+          </div>
+          <a
+            href="https://docs.passport.gitcoin.co/building-with-passport/passport-api/getting-access#getting-your-scorer-id"
+            class="text-blue-900 font-medium text-bold"
+          >
+            Get Your ScoreId here!
+          </a>
+
+          <span id="userAddress" className="text-gray-700 mb-4"></span>
+          <div className="flex space-x-4">
+            {/* Blog Content form */}
+            {eligible && (
+              <div className="flex flex-col text-black">
+                <input
+                  type="text"
+                  id="blogTitle"
+                  placeholder="Blog Title"
+                  // onChange={(event) => handleInputChange(event, setBlogTitle)}
+                  className="rounded-md border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500 mb-2"
+                  required
+                />
+                <input
+                  type="text"
+                  id="blogCategory"
+                  placeholder="Blog Category"
+                  // onChange={(event) => handleInputChange(event, setBlogCategory)}
+                  className="rounded-md border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500 mb-2"
+                  required
+                />
+                <input
+                  type="text"
+                  id="blogContent"
+                  placeholder="Blog Content"
+                  className="rounded-md border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500 mb-2"
+                  style={{ minHeight: "100px" }}
+                  required
+                />
+                <button
+                  id="sendMailButton"
+                  className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  onClick={attestBlog}
+                >
+                  Attest Blog
+                </button>
+              </div>
+            )}
           </div>
         </div>
 
