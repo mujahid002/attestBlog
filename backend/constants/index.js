a394e44c9010f60de845a630aac53bbefe90c01e
Mujahid002 <mujahidshaik2002@gmail.com>
2024-05-03 17:53:02 +0530

    added admin address for granting posts

---
+++ backend/constants/index.js
@@ -0,0 +1,221 @@
+require("dotenv").config();
+const ethers = require("ethers");
+
+const provider = new ethers.JsonRpcProvider(
+  `${process.env.OP_SEPOLIA_RPC_URL}`
+);
+const opSepWallet = new ethers.Wallet(
+  `${process.env.ADMIN_PRIVATE_KEY}`,
+  provider
+);
+
+const RESOLVER_CONTRACT_ADDRESS = "0xFF79C42512003C04aA2Dc736be9F22eb60590c1f";
+
+const RESOLVER_CONTRACT_ABI = [
+  {
+    inputs: [{ internalType: "contract IEAS", name: "_eas", type: "address" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  { inputs: [], name: "AccessDenied", type: "error" },
+  { inputs: [], name: "InsufficientValue", type: "error" },
+  { inputs: [], name: "InvalidEAS", type: "error" },
+  { inputs: [], name: "InvalidLength", type: "error" },
+  { inputs: [], name: "NotPayable", type: "error" },
+  {
+    inputs: [{ internalType: "address", name: "owner", type: "address" }],
+    name: "OwnableInvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "bytes32", name: "uid", type: "bytes32" },
+          { internalType: "bytes32", name: "schema", type: "bytes32" },
+          { internalType: "uint64", name: "time", type: "uint64" },
+          { internalType: "uint64", name: "expirationTime", type: "uint64" },
+          { internalType: "uint64", name: "revocationTime", type: "uint64" },
+          { internalType: "bytes32", name: "refUID", type: "bytes32" },
+          { internalType: "address", name: "recipient", type: "address" },
+          { internalType: "address", name: "attester", type: "address" },
+          { internalType: "bool", name: "revocable", type: "bool" },
+          { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        internalType: "struct Attestation",
+        name: "attestation",
+        type: "tuple",
+      },
+    ],
+    name: "attest",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "isPayable",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "bytes32", name: "uid", type: "bytes32" },
+          { internalType: "bytes32", name: "schema", type: "bytes32" },
+          { internalType: "uint64", name: "time", type: "uint64" },
+          { internalType: "uint64", name: "expirationTime", type: "uint64" },
+          { internalType: "uint64", name: "revocationTime", type: "uint64" },
+          { internalType: "bytes32", name: "refUID", type: "bytes32" },
+          { internalType: "address", name: "recipient", type: "address" },
+          { internalType: "address", name: "attester", type: "address" },
+          { internalType: "bool", name: "revocable", type: "bool" },
+          { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        internalType: "struct Attestation[]",
+        name: "attestations",
+        type: "tuple[]",
+      },
+      { internalType: "uint256[]", name: "values", type: "uint256[]" },
+    ],
+    name: "multiAttest",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "bytes32", name: "uid", type: "bytes32" },
+          { internalType: "bytes32", name: "schema", type: "bytes32" },
+          { internalType: "uint64", name: "time", type: "uint64" },
+          { internalType: "uint64", name: "expirationTime", type: "uint64" },
+          { internalType: "uint64", name: "revocationTime", type: "uint64" },
+          { internalType: "bytes32", name: "refUID", type: "bytes32" },
+          { internalType: "address", name: "recipient", type: "address" },
+          { internalType: "address", name: "attester", type: "address" },
+          { internalType: "bool", name: "revocable", type: "bool" },
+          { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        internalType: "struct Attestation[]",
+        name: "attestations",
+        type: "tuple[]",
+      },
+      { internalType: "uint256[]", name: "values", type: "uint256[]" },
+    ],
+    name: "multiRevoke",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "bytes32", name: "uid", type: "bytes32" },
+          { internalType: "bytes32", name: "schema", type: "bytes32" },
+          { internalType: "uint64", name: "time", type: "uint64" },
+          { internalType: "uint64", name: "expirationTime", type: "uint64" },
+          { internalType: "uint64", name: "revocationTime", type: "uint64" },
+          { internalType: "bytes32", name: "refUID", type: "bytes32" },
+          { internalType: "address", name: "recipient", type: "address" },
+          { internalType: "address", name: "attester", type: "address" },
+          { internalType: "bool", name: "revocable", type: "bool" },
+          { internalType: "bytes", name: "data", type: "bytes" },
+        ],
+        internalType: "struct Attestation",
+        name: "attestation",
+        type: "tuple",
+      },
+    ],
+    name: "revoke",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "s_attesterAccess",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "attester", type: "address" },
+      { internalType: "bool", name: "access", type: "bool" },
+    ],
+    name: "updateAccessForAttester",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "version",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  { stateMutability: "payable", type: "receive" },
+];
+const resolverContract = new ethers.Contract(
+  RESOLVER_CONTRACT_ADDRESS,
+  RESOLVER_CONTRACT_ABI,
+  opSepWallet
+);
+
+module.exports = {
+  provider,
+  RESOLVER_CONTRACT_ADDRESS,
+  RESOLVER_CONTRACT_ABI,
+  resolverContract,
+};
