9ee8bee875fa6197deeecfdf8e0c1c38e4e9de13
Mujahid002 <mujahidshaik2002@gmail.com>
2024-07-18 20:01:57 +0530

    updated script for DX Resolver

--- backend/hardhat/scripts/deploy.js
+++ backend/hardhat/scripts/deploy.js
@@ -1,22 +1,39 @@
 const { ethers, upgrades, run } = require("hardhat");
 
 const main = async () => {
-  // Knowledge Token
-  const postComment = await hre.ethers.getContractFactory("PostComment");
+  // const postComment = await hre.ethers.getContractFactory("PostComment");
+  // const easAddress = "0x4200000000000000000000000000000000000021";
+  // console.log("Deploying PostComment Contract...");
+  // const PostComment = await postComment.deploy(easAddress, {
+  //   gasPrice: 30000000000,
+  // });
+  // await PostComment.waitForDeployment();
+  // const postCommentAddress = await PostComment.getAddress();
+  // console.log("PostComment Contract Address:", postCommentAddress);
+  // console.log("----------------------------------------------------------");
+
+  // // Verify PostComment Contract
+  // console.log("Verifying PostComment...");
+  // await run("verify:verify", {
+  //   address: postCommentAddress,
+  //   constructorArguments: [easAddress],
+  // });
+  // console.log("----------------------------------------------------------");
+  const dxResolver = await hre.ethers.getContractFactory("DxResolver");
   const easAddress = "0x4200000000000000000000000000000000000021";
-  console.log("Deploying PostComment Contract...");
-  const PostComment = await postComment.deploy(easAddress, {
+  console.log("Deploying dxResolver Contract...");
+  const DxResolver = await dxResolver.deploy(easAddress, {
     gasPrice: 30000000000,
   });
-  await PostComment.waitForDeployment();
-  const postCommentAddress = await PostComment.getAddress();
-  console.log("PostComment Contract Address:", postCommentAddress);
+  await DxResolver.waitForDeployment();
+  const dxResolverAddress = await DxResolver.getAddress();
+  console.log("DxResolver Contract Address:", dxResolverAddress);
   console.log("----------------------------------------------------------");
 
-  // Verify PostComment Contract
-  console.log("Verifying PostComment...");
+  // Verify DxResolver Contract
+  console.log("Verifying DxResolver...");
   await run("verify:verify", {
-    address: postCommentAddress,
+    address: dxResolverAddress,
     constructorArguments: [easAddress],
   });
   console.log("----------------------------------------------------------");
